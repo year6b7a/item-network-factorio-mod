e787780bdcda51cf8e19ff943f26b0d6381f2b53
Ben Gardner <gardner.ben@gmail.com>
2023-08-11 17:07:15 -0500

    Change the Network View to use tabs instead of radio buttons
    Also adds a close button.
    Haven't figured out how to make it draggable.

--- src/NetworkViewUi.lua
+++ src/NetworkViewUi.lua
@@ -6,6 +6,16 @@ local M = {}
 M.WIDTH = 490
 M.HEIGHT = 500
 
+-- Builds the GUI for the item, fluid, and shortage tabs.
+local function build_item_page(parent)
+  local main_flow = parent.add({
+    type = "flow",
+    direction = "vertical",
+  })
+
+  return main_flow
+end
+
 function M.open_main_frame(player_index)
   local ui = GlobalState.get_ui_state(player_index)
   if ui.net_view ~= nil then
@@ -19,12 +29,34 @@ function M.open_main_frame(player_index)
   end
 
   local width = M.WIDTH
-  local height = M.HEIGHT
+  local height = M.HEIGHT + 32
 
+  --[[
+  I want the GUI to look like this:
+
+  +--------------------------------------------------+
+  | Network View ||||||||||||||||||||||||||||| [R][X]|
+  +--------------------------------------------------+
+  | Items | Fluids | Shortages |                     | <- tabs
+  +--------------------------------------------------+
+  | [I]  [I]  [I]  [I]  [I]  [I]  [I]  [I]  [I]  [I] | <- content
+    ... repeated ...
+  | [I]  [I]  [I]  [I]  [I]  [I]  [I]  [I]  [I]  [I] |
+  +--------------------------------------------------+
+
+  [R] is refresh button and [X] is close. [I] are item icons with the number overlay.
+  I want the ||||| stuff to make the window draggable.
+  Right now, I can get it to look right, but it isn't draggable.
+  OR I can omit the [R][X] buttons make it draggable.
+  ]]
+
+  -- create the main window
   local frame = player.gui.screen.add({
     type = "frame",
     name = UiConstants.NV_FRAME,
-    caption = "Network View",
+    -- enabling the frame caption enables dragging, but
+    -- doesn't allow the buttons to be on the top line
+    --caption = "Network View",
   })
   player.opened = frame
   frame.style.size = { width, height }
@@ -38,46 +70,70 @@ function M.open_main_frame(player_index)
   local header_flow = main_flow.add({
     type = "flow",
     direction = "horizontal",
+    style = "dialog_buttons_horizontal_flow"
   })
 
-  local item_radio = header_flow.add({
-    type = "radiobutton",
-    state = true,
-    tags = { event = UiConstants.NV_ITEM_RADIO },
-  })
-  header_flow.add({ type = "label", caption = "Items" })
+  header_flow.add{
+    type = "label",
+    caption = "Network View",
+    style = "frame_title",
+  }
 
-  local fluid_radio = header_flow.add({
-    type = "radiobutton",
-    state = false,
-    tags = { event = UiConstants.NV_FLUID_RADIO },
-  })
-  header_flow.add({ type = "label", caption = "Fluids" })
+  header_flow.add{
+    type = "empty-widget",
+    --style = "draggable_space_header"
+    ignored_by_interaction = true,
+    style = "flib_titlebar_drag_handle"
+  }
 
-  local shortage_radio = header_flow.add({
-    type = "radiobutton",
-    state = false,
-    tags = { event = UiConstants.NV_SHORTAGE_RADIO },
-  })
-  header_flow.add({ type = "label", caption = "Shortages" })
-
-  header_flow.add({
-    type = "button",
-    caption = "Refresh",
+  header_flow.add{
+    type = "sprite-button",
+    sprite = 'utility/refresh',
+    style = "frame_action_button",
+    tooltip = { "gui.refresh" },
     tags = { event = UiConstants.NV_REFRESH_BTN },
-  })
+  }
+
+  header_flow.add{
+    type = "sprite-button",
+    sprite = 'utility/close_white',
+    hovered_sprite = 'utility/close_black',
+    clicked_sprite = 'utility/close_black',
+    style = "close_button",
+    tags = { event = UiConstants.NV_CLOSE_BTN },
+  }
+
+  -- add tabbed stuff
+  local tabbed_pane = main_flow.add{
+    type = "tabbed-pane",
+    tags = { event = UiConstants.NV_TABBED_PANE },
+  }
+
+  local tab_item = tabbed_pane.add{type="tab", caption="Items"}
+  local tab_fluid = tabbed_pane.add{type="tab", caption="Fluids"}
+  local tab_shortage = tabbed_pane.add{type="tab", caption="Shortages"}
+
+  tabbed_pane.add_tab(tab_item, build_item_page(tabbed_pane))
+  tabbed_pane.add_tab(tab_fluid, build_item_page(tabbed_pane))
+  tabbed_pane.add_tab(tab_shortage, build_item_page(tabbed_pane))
+
+  -- select "items" (not really needed, as that is the default)
+  tabbed_pane.selected_tab_index = 1
 
   ui.net_view = {
     frame = frame,
-    main_flow = main_flow,
-    view_type = "item",
-    item_radio = item_radio,
-    fluid_radio = fluid_radio,
-    shortage_radio = shortage_radio,
+    tabbed_pane = tabbed_pane,
   }
 
   M.update_items(player_index)
 end
+
+-- used when setting the active tab
+local tab_idx_to_view_type = {
+  "item",
+  "fluid",
+  "shortage",
+}
 
 function M.update_items(player_index)
   local ui = GlobalState.get_ui_state(player_index)
@@ -85,12 +141,20 @@ function M.update_items(player_index)
   if net_view == nil then
     return
   end
-  local item_flow = net_view.main_flow[UiConstants.NV_ITEM_FLOW]
+  local tabbed_pane = net_view.tabbed_pane
+
+  net_view.view_type = tab_idx_to_view_type[tabbed_pane.selected_tab_index]
+  local main_flow = tabbed_pane.tabs[tabbed_pane.selected_tab_index].content
+  if main_flow == nil then
+    return
+  end
+
+  local item_flow = main_flow[UiConstants.NV_ITEM_FLOW]
   if item_flow ~= nil then
     item_flow.destroy()
   end
 
-  item_flow = net_view.main_flow.add({
+  item_flow = main_flow.add({
     type = "scroll-pane",
     direction = "vertical",
     name = UiConstants.NV_ITEM_FLOW,
